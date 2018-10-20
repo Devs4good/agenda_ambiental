b0207dba3b1bb18b732a26964e6fa62bc52be6a7
hanss <hanssbcr@gmail.com>
2018-10-20 11:00:52 -0300

    about

---
+++ web/js/main.js
@@ -0,0 +1,18 @@
+$(function() { var logo = $(".log"); 
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    
+    if (scroll >= 140) {
+        if(!logo.hasClass("line-logo")) {
+          logo.hide();
+          logo.removeClass('log').addClass("line-logo").fadeIn( "slow");
+        }
+      } else {
+        if(!logo.hasClass("log")) {
+          logo.hide();
+          logo.removeClass("line-logo").addClass('log').fadeIn( "slow");
+        }
+      }
+  
+  });
+  });
